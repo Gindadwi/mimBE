57d39c76cd555a9fefe8071200e24f6728a9126a
Gindadwi <gindadwi144@gmail.com>
2025-03-05 06:15:00 -0800

    app

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+// require("dotenv").config();
+// const express = require("express");
+// const app = express();
+// const db = require("./models");
+
+// app.use(express.json());
+
+// // Routing untuk user dan spp
+// const userRoutes = require("./routes/userRoutes");
+// const sppRoutes = require("./routes/sppRoutes");
+
+// app.use("/api/users", userRoutes);
+// app.use("/api/spp", sppRoutes);
+
+// const PORT = process.env.PORT || 3000;
+// db.sequelize.sync().then(() => {
+//   app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+//   });
+// });
+
+// module.exports = app;
+
 require("dotenv").config();
 const express = require("express");
 const app = express();
@@ -12,9 +35,7 @@ const sppRoutes = require("./routes/sppRoutes");
 app.use("/api/users", userRoutes);
 app.use("/api/spp", sppRoutes);
 
-const PORT = process.env.PORT || 3000;
-db.sequelize.sync().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-});
+// Hapus app.listen() agar bisa jalan di Vercel
+db.sequelize.sync();
+
+module.exports = app; // <-- Harus diexport
