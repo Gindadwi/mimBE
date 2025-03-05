dc607fa19ccbe93ba36f3972a54d97b5c7aef5e6
Gindadwi <gindadwi144@gmail.com>
2025-03-05 04:58:45 -0800

    config

--- config/config.js
+++ config/config.js
@@ -2,18 +2,18 @@ require("dotenv").config(); // Pastikan dotenv dipanggil jika menggunakan .env
 
 module.exports = {
   development: {
-    username: "root",
-    password: null,
-    database: "bemim",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    username: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
   },
   test: {
-    username: "root",
-    password: null,
-    database: "bemim",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    username: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
   },
   production: {
     username: process.env.MYSQLUSER,
